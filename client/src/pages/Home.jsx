654b89646da2a99babf2fa524839ed5c31156d71
Vinay <vinayratnakar2003@gmail.com>
2025-08-12 18:31:02 +0530

    first commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AiTools from '../components/AiTools'
-import Testimonial from '../components/Testimonial'
 import Plan from '../components/Plan'
 import Footer from '../components/Footer'
 
